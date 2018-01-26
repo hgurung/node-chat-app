3924779b79fd7d83738de9587bde0f8d1c9720c9
Harris Gurung <harris@Harriss-Mac-mini.local>
2018-01-26 16:46:28 +0545

    Styles and jquery added for form

--- public/js/index.js
+++ public/js/index.js
@@ -28,11 +28,12 @@ socket.on('newLocationMessage', function(message){
 
 $('#message-form').on('submit',function(e) {
     e.preventDefault();
+    var messageTextBox = $('[name=message]');
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
+        text: messageTextBox.val()
     }, function(data) {
-        console.log(data);
+        messageTextBox.val('');
     });
 });
 
@@ -41,12 +42,17 @@ locationBtn.on('click', function(e){
     if(!navigator.geolocation) {
         return alert('Gelolcation not supported by your browser');
     }
+
+    locationBtn.attr('disabled','disabled').text('Sending Location...');
+
     navigator.geolocation.getCurrentPosition(function(position){
+        locationBtn.removeAttr('disabled').text('Send Location');
         socket.emit('createLocationMessage',{
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     },function() {
+        locationBtn.removeAttr('disabled').text('Send Location');
         alert('Unable to fetch location');
     });
 });
--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ io.on('connection', (socket) => {
     socket.on('createMessage', (message, callback) => {
         console.log('Create message', message);
         io.emit('newMessage',generateMessage(message.from,message.text));
-        callback('This is from the server.');
+        callback();
     });
 
     socket.on('createLocationMessage',(coords) => {
